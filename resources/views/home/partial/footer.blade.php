278d244121780b4b15f69d9567bf247ccbd6f37d
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-03-26 14:19:43 +0100

    comment

--- resources/views/home/partial/footer.blade.php
+++ resources/views/home/partial/footer.blade.php
@@ -79,7 +79,7 @@
     <div class="row">
       <div class="col-md-12 text-center">
         <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Designed <i class="icon-heart" aria-hidden="true"></i> by <a href="" target="_blank">Tyneside Innovation</a>
+        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Designed <i class="icon-heart" aria-hidden="true"></i> by <a href="https://tynesideinnovation.com" target="_blank">Tyneside Innovation</a>
         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
       </div>
     </div>
