9ed6ec001a176a2251949e08158081a9fb54de75
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-11-24 10:00:53 +0100

    admin

--- resources/views/admin/partials/navbar.blade.php
+++ resources/views/admin/partials/navbar.blade.php
@@ -489,7 +489,7 @@
 
                              </a>
                             <div class="dropdown-menu dropdown-menu-end">
-                                <a href="app-profile.html" class="dropdown-item ai-icon">
+                                {{-- <a href="app-profile.html" class="dropdown-item ai-icon">
                                     <svg id="icon-user1" xmlns="http://www.w3.org/2000/svg" class="text-primary"
                                         width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                         stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
@@ -497,18 +497,8 @@
                                         <circle cx="12" cy="7" r="4"></circle>
                                     </svg>
                                     <span class="ms-2">Profile </span>
-                                </a>
-                                <a href="email-inbox.html" class="dropdown-item ai-icon">
-                                    <svg id="icon-inbox" xmlns="http://www.w3.org/2000/svg" class="text-success"
-                                        width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                        <path
-                                            d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z">
-                                        </path>
-                                        <polyline points="22,6 12,13 2,6"></polyline>
-                                    </svg>
-                                    <span class="ms-2">Inbox </span>
-                                </a>
+                                </a> --}}
+                              
                                
                               
                                 <a href="{{ route('admin.logout') }}"  
